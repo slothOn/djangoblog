1e99ccf7818b686a99d11f1738555aae7d2f529d
slothOn <zxctyzx@163.com>
2015-09-10 20:30:04 +0800

    git detail init

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'article.views.home'),
-    url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
     url(r'^test/$', 'article.views.test'),
 ]
